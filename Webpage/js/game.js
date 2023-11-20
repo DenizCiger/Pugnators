32974403b4ad85b7612ac881e7b9b5b80abdf68c
DenizCiger <deniz.ciger@gmx.at>
2023-11-20 06:29:47 +0100

    Add Turbowarp Prototype

--- Webpage/js/game.js
+++ Webpage/js/game.js
@@ -3,8 +3,8 @@ window.onload = function() {
         type: Phaser.AUTO,
         mode: Phaser.Scale.Fit,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: 192,
+        height: 108,
         scene: {
             preload: preload,
             create: create
@@ -19,5 +19,13 @@ window.onload = function() {
 
     function create() {
         this.add.image('background').setOrigin(0).setDisplaySize(window.innerWidth, window.innerHeight);
+        // Calculate the position for the centered rectangle
+        const rectWidth = config.width / 2;
+        const rectHeight = config.height / 2;
+        const rectX = config.width / 2;
+        const rectY = config.height - (rectHeight/2) - 100;
+
+        // Create a rectangle and add it to the scene
+        const rectangle = this.add.rectangle(rectX, rectY, rectWidth, rectHeight, 0x00ff00);
     }
 }
