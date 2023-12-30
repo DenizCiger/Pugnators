3e23a1b7e226d2dc34a89bdb86f030e930ea27e7
DenizCiger <deniz.ciger@gmx.at>
2023-12-30 21:49:41 +0100

    Fix responsiveness of canvas

--- Webpage/js/game.js
+++ Webpage/js/game.js
@@ -1,9 +1,6 @@
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext('2d');
 
-canvas.width = 1920;
-canvas.height = 1080;
-
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 const gravity = 0.2;
