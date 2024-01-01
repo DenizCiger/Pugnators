f10eb4d1f1043a850bb12ae9c4d35c48ef2e34d8
DenizCiger <deniz.ciger@gmx.at>
2024-01-01 22:50:39 +0100

    Moonwalk :)

--- Webpage/js/characterData.js
+++ Webpage/js/characterData.js
@@ -43,7 +43,7 @@ const characterData = {
             actionName: 'idle',
             isAttack: false,
             animationSrc: './images/Game-Textures/Characters/Nerd/animations/walk.png',
-            numberOfFrames: 12,
+            numberOfFrames: 10,
             offset: { x: 10, y: 0 },
             framesHold: 5
             // Other ability details
