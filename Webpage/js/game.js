b307029246b401ce7f70be6d0b976c69f0c9dc55
DenizCiger <deniz.ciger@gmx.at>
2024-01-03 22:24:55 +0100

    Move Util functions

--- Webpage/js/game.js
+++ Webpage/js/game.js
@@ -46,6 +46,7 @@ function animate() {
   updatePercentageDisplays();
 }
 
+// Setup functions
 function setup() {
   setupDisplays();
   setupBackground();
@@ -60,10 +61,6 @@ function setupDisplays() {
   playerInfoDisplays = getDisplayElements('playerInfo', 4);
   playerIconDisplays = getDisplayElements('icon', 4);
   percentageDisplays = getDisplayElements('percentage', 4);
-}
-
-function getDisplayElements(elementName, count) {
-  return Array.from({ length: count }, (_, i) => document.getElementById(`${elementName}${i + 1}`));
 }
 
 function setupBackground() {
@@ -96,57 +93,9 @@ function setupPlayers() {
   });
 }
 
-function getPlayerColor(index) {
-  const colors = ['red', 'blue', 'green', 'yellow'];
-  return colors[index] || 'white';
-}
-
-function hideNonExistentPlayers() {
-  for (let i = players.length; i < playerInfoDisplays.length; i++) {
-    playerInfoDisplays[i].style.display = 'none';
-  }
-}
-
 function setupUserIcons() {
   for (let i = 0; i < players.length; i++) {
     playerIconDisplays[i].src = characterData[playerCharacters[i]].find(a => a.actionName === 'info').logoSrc;
-  }
-}
-
-function updateAnimations() {
-  window.requestAnimationFrame(animate);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  background.update();
-  players[0].logCoords()
-
-  for (let i = 0; i < players.length; i++) {
-    players[i].update();
-  }
-
-  for (let i = 0; i < map.length; i++) {
-    map[i].update();
-  }
-}
-
-function updatePercentageDisplays() {
-  for (let i = 0; i < players.length; i++) {
-    percentageDisplays[i].textContent = players[i].percentage + "%";
-    percentageDisplays[i].style.color = getColorForPercentage(players[i].percentage);
-  }
-}
-
-function getColorForPercentage(percentage) {
-  switch (true) {
-    case percentage >= 200:
-      return '#5A0000'; // Dark red for percentages >= 200
-    case percentage > 100:
-      return '#CA0000'; // Red for percentages > 100
-    case percentage > 70:
-      return '#FFA500'; // Orange for percentages > 70
-    case percentage > 35:
-      return '#FFFF00'; // Yellow for percentages > 35
-    default:
-      return '#FFFFFF'; // White for percentages <= 35
   }
 }
 
---
+++ Webpage/js/util.js
@@ -0,0 +1,52 @@
+function getPlayerColor(index) {
+    const colors = ['red', 'blue', 'green', 'yellow'];
+    return colors[index] || 'white';
+  }
+  
+  function hideNonExistentPlayers() {
+    for (let i = players.length; i < playerInfoDisplays.length; i++) {
+      playerInfoDisplays[i].style.display = 'none';
+    }
+  }
+  
+  function updateAnimations() {
+    window.requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background.update();
+    players[0].logCoords()
+  
+    for (let i = 0; i < players.length; i++) {
+      players[i].update();
+    }
+  
+    for (let i = 0; i < map.length; i++) {
+      map[i].update();
+    }
+  }
+  
+  function updatePercentageDisplays() {
+    for (let i = 0; i < players.length; i++) {
+      percentageDisplays[i].textContent = players[i].percentage + "%";
+      percentageDisplays[i].style.color = getColorForPercentage(players[i].percentage);
+    }
+  }
+  
+  function getColorForPercentage(percentage) {
+    switch (true) {
+      case percentage >= 200:
+        return '#5A0000'; // Dark red for percentages >= 200
+      case percentage > 100:
+        return '#CA0000'; // Red for percentages > 100
+      case percentage > 70:
+        return '#FFA500'; // Orange for percentages > 70
+      case percentage > 35:
+        return '#FFFF00'; // Yellow for percentages > 35
+      default:
+        return '#FFFFFF'; // White for percentages <= 35
+    }
+  }
+  
+  function getDisplayElements(elementName, count) {
+    return Array.from({ length: count }, (_, i) => document.getElementById(`${elementName}${i + 1}`));
+  }
+  
