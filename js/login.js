680104306684e26c68280e0bb9c65bd505d89765
DenizCiger <deniz.ciger@gmx.at>
2024-03-09 15:03:50 +0100

    Import firebase module

--- js/login.js
+++ js/login.js
@@ -1,3 +1,5 @@
+import { initializeApp, getAnalytics } from "firebase/app";
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -9,96 +11,10 @@ const firebaseConfig = {
     appId: "1:942522410655:web:fd58ed131813d85e3ca4aa",
     measurementId: "G-7T45QC64CD"
 };
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-// Initialize Variables
-const auth = firebase.auth();
-const database = firebase.database();
-
-// Set up our register function
-function register() {
-    // Get all our input fields
-    email = document.getElementById('email').value;
-    password = document.getElementById('password').value;
-    username = document.getElementById('username').value;
-    // Validate input fields
-    if (validate_email(email) == false) {
-        alert('Your email is invalid!');
-        return;
-        // Don't continue running the code
-    }
-    if (validate_password(password) == false) {
-        alert('Your password is invalid! It must be at least 6 characters long!');
-        return;
-        // Don't continue running the code
-    }
-    if (validate_field(username) == false) {
-        alert('Invalid username! Please enter a username.');
-        return;
-    }
-    // Move on with Auth
-    auth.createUserWithEmailAndPassword(email, password)
-        .then(function() {
-            // Declare user variable
-            var user = auth.currentUser;
-            // Add this user to Firebase Database
-            var database_ref = database.ref();
-            // Create User Data
-            var user_data = {
-                email: email,
-                username: username,
-                last_login: Date.now()
-            };
-            // Push to Firebase Database
-            database_ref.child('users/' + user.uid).set(user_data);
-            alert('User Created!!');
-        })
-        .catch(function(error) {
-            // Firebase will use this to alert of its errors
-            var error_code = error.code;
-            var error_message = error.message;
-            alert(error_message);
-        });
-}
-
-// Set up our login function
-function login() {
-    // Get all our input fields
-    email = document.getElementById('email').value;
-    password = document.getElementById('password').value;
-    // Validate input fields
-    if (validate_email(email) == false) {
-        alert('Your email is invalid!');
-        return;
-        // Don't continue running the code
-    }
-    if (validate_password(password) == false) {
-        alert('Your password is invalid! It must be at least 6 characters long!');
-        return;
-        // Don't continue running the code
-    }
-    auth.signInWithEmailAndPassword(email, password)
-        .then(function() {
-            // Declare user variable
-            var user = auth.currentUser;
-            // Add this user to Firebase Database
-            var database_ref = database.ref();
-            // Create User Data
-            var user_data = {
-                last_login: Date.now()
-            };
-            // Push to Firebase Database
-            database_ref.child('users/' + user.uid).update(user_data);
-            alert('User Logged In!');
-        })
-        .catch(function(error) {
-            // Firebase will use this to alert of its errors
-            var error_code = error.code;
-            var error_message = error.message;
-            alert(error_message);
-        });
-}
 
 function validate_email(email) {
     expression = /^[^@]+@\w+(\.\w+)+\w$/;
