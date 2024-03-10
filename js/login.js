77b19e958a7fa0d9b56e6c7662f2aa53fbbec721
DenizCiger <deniz.ciger@gmx.at>
2024-03-10 17:59:45 +0100

    Refactor Firebase initialization and add user registration and login functionality

--- js/login.js
+++ js/login.js
@@ -1,48 +1,90 @@
-import { initializeApp, getAnalytics } from "firebase/app";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("firebase/app"));
+require("firebase/auth");
+const auth_1 = require("firebase/auth");
+require("firebase/database");
+const database_1 = require("firebase/database");
 const firebaseConfig = {
     apiKey: "AIzaSyCbcBZfwy0N9luIE9gnONuOLxNkuyV8amI",
     authDomain: "pugnators-1821a.firebaseapp.com",
+    databaseURL: "https://pugnators-1821a-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "pugnators-1821a",
     storageBucket: "pugnators-1821a.appspot.com",
     messagingSenderId: "942522410655",
     appId: "1:942522410655:web:fd58ed131813d85e3ca4aa",
     measurementId: "G-7T45QC64CD"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+const app = app_1.default.initializeApp(firebaseConfig);
+const auth = (0, auth_1.getAuth)(app);
+const database = (0, database_1.getDatabase)(app);
+console.log(auth, database);
+function register() {
+    let usernameElement = document.getElementById('username');
+    let emailElement = document.getElementById('email');
+    let passwordElement = document.getElementById('password');
+    let username = usernameElement ? usernameElement.value : '';
+    let email = emailElement ? emailElement.value : '';
+    let password = passwordElement ? passwordElement.value : '';
+    if (!validate_username(username) || !validate_email(email) || !validate_password(password))
+        return;
+    (0, auth_1.createUserWithEmailAndPassword)(auth, email, password)
+        .then((userCredential) => {
+        const user = userCredential.user;
+        alert(`${user} Signed Up!`);
+    })
+        .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(`${errorMessage} Code: ${errorCode}`);
+    });
+}
+function login() {
+    let emailElement = document.getElementById('email');
+    let passwordElement = document.getElementById('password');
+    let email = emailElement ? emailElement.value : '';
+    let password = passwordElement ? passwordElement.value : '';
+    if (!validate_email(email) || !validate_password(password))
+        return;
+    (0, auth_1.signInWithEmailAndPassword)(auth, email, password)
+        .then((userCredential) => {
+        const user = userCredential.user;
+        alert(`${user} Signed In!`);
+    })
+        .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(`${errorMessage} Code: ${errorCode}`);
+    });
+}
 function validate_email(email) {
-    expression = /^[^@]+@\w+(\.\w+)+\w$/;
-    if (expression.test(email) == true) {
-        // Email is good
+    const expression = /^[^@]+@\w+(\.\w+)+\w$/;
+    if (expression.test(email)) {
         return true;
-    } else {
-        // Email is not good
+    }
+    else {
+        alert('Invalid email format');
         return false;
     }
 }
-
 function validate_password(password) {
-    // Firebase only accepts lengths greater than 6
-    if (password < 6) {
+    if (password.length < 6) {
+        alert('Password must be at least 6 characters long');
         return false;
-    } else {
+    }
+    else {
         return true;
     }
 }
-
-function validate_field(field) {
-    if (field == null) {
+function validate_username(username) {
+    if (username.length < 3) {
+        alert('Username must be at least 3 characters long');
         return false;
     }
-    if (field.length <= 0) {
-        return false;
-    } else {
+    else {
         return true;
     }
 }
