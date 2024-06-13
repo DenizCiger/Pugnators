dbcc729041bd0c4eba09e7652437ef11114f9101
DenizCiger <deniz.ciger@gmx.at>
2024-06-13 22:30:47 +0200

    Add canvas for game rendering, handle key presses, add the first physics updates

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,189 @@
+// Websocket server for handling key presses and releases
 const PORT = 8443;
-
 const http = require('http').createServer();
-
 const io = require('socket.io')(http, {
     cors: { origin: '*' }
 });
 
+// Game classes
+class Fighter {
+    constructor({
+        characterType,
+        position = { x: 0, y: 0 },
+        color
+    }) {
+        this.characterType = characterType;
+        this.position = position;
+        this.color = color;
+        this.pressedKeys = {
+            space: false,
+            up: false,
+            left: false,
+            down: false,
+            right: false
+        };
+        this.width = 20;
+        this.height = 60;
+        this.moveVelos = {
+            x: 0,
+            y: 0
+        };
+    }
+
+    collidesWith(other) {
+        for (const element of other) {
+            if (this.position.x < element.position.x + element.width &&
+                this.position.x + this.width > element.position.x &&
+                this.position.y < element.position.y + element.height &&
+                this.position.y + this.height > element.position.y) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+class Obstacle {
+    constructor({
+        position = { x: 0, y: 0 },
+        color,
+        width = 200,
+        height = 20
+    }) {
+        this.position = position;
+        this.color = color;
+        this.width = width;
+        this.height = height;
+    }
+}
+
+// Game variables
+let sockets = [];
+let players = [];
+let obstacles = [
+    new Obstacle({
+        position: { x: 100, y: 200 },
+        color: 'black',
+    }),
+];
+const colors = ['blue', 'red', 'green', 'yellow', 'purple', 'orange'];
+const moveVeloConsts = {
+    jump:       { x:  0,    y: -15 },
+    gravity:    { x:  0,    y:   1 },
+    walk:      { x:  4,    y:   0 },
+};
+const maxSpeeds = {
+    fall:      10,
+    walk:       8,
+};
+
+// Handle incoming connections
 io.on('connection', (socket) => {
     console.log('A user connected');
-    socket.on('message', (data) => {
-        const { username, message } = data;
-        console.log(`${username} sent => ${message}`);
-        io.emit('message', `${username} sent => ${message}`);
+    sockets[socket.id] = socket;
+    players[socket.id] = new Fighter({
+        characterType: 'Nerd',
+        position: { x: 0, y: 0 },
+        color: colors[Math.floor(Math.random() * colors.length)]
     });
+
+    // Handle disconnections
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+        delete sockets[socket.id];
+        delete players[socket.id];
+    });
+    
+    
+    // Handle incoming key presses
+    socket.on('keyPressUpdate', (pressedKeys) => {
+        players[socket.id].pressedKeys = pressedKeys;
+    });
+
 });
 
+// Update
+setInterval(() => {
+    const pack = [];
+
+    // Update player physics
+    for (const player of Object.values(players)) {
+        // Walk handling
+
+        if (player.pressedKeys.left != player.pressedKeys.right) {  
+            if (player.pressedKeys.left) {
+                if (player.moveVelos.x > -maxSpeeds.walk) {
+                    player.moveVelos.x -= moveVeloConsts.walk.x;
+                } else {
+                    player.moveVelos.x = -maxSpeeds.walk;
+                }
+            }
+            if (player.pressedKeys.right) {
+                if (player.moveVelos.x < maxSpeeds.walk) {
+                    player.moveVelos.x += moveVeloConsts.walk.x;
+                } else {
+                    player.moveVelos.x = maxSpeeds.walk;
+                }
+            }
+        } else {
+            if (Math.abs(player.moveVelos.x) < 0.1) {
+                player.moveVelos.x = 0;
+            } else {
+                player.moveVelos.x /= 1.1;
+            }
+        }
+
+        // Gravity handling
+        if (player.moveVelos.y < maxSpeeds.fall) {
+            player.moveVelos.y += moveVeloConsts.gravity.y;
+        } else {
+            player.moveVelos.y = maxSpeeds.fall;
+        }
+
+        player.position.x += player.moveVelos.x;
+        player.position.y += player.moveVelos.y;
+
+        // Out of bounds handling
+        if (player.position.x > 480) {
+            player.position.x = -20;
+        }
+        if (player.position.y > 270) {
+            player.position.y = -20;
+        }
+        if (player.position.x < -20) {
+            player.position.x = 480;
+        }
+        if (player.position.y < -20) {
+            player.position.y = 270;
+        }
+
+        // Saving relevant player data
+        pack.push({
+            position: player.position,
+            color: player.color,
+            width: player.width,
+            height: player.height
+        });
+    }
+
+    for (const obstacle of obstacles) {
+        pack.push({
+            position: obstacle.position,
+            color: obstacle.color,
+            width: obstacle.width,
+            height: obstacle.height
+        });
+    }
+
+    for (const socket of Object.values(sockets)) {
+        socket.emit('update', pack);
+    }
+}, 1000/60);
+
+
+
+
+// Start the server
 http.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
--- js/client.js
+++ js/client.js
@@ -1,17 +1,70 @@
 const socket = io('https://socket.wohlschlager.net:443');
 
-socket.on('message', text => {
+// Game variables
+const ctx = document.getElementById('game').getContext('2d');
+const availableKeys = {
+    space: 32,  // Space
+    up: 87,     // W
+    left: 65,   // A
+    down: 83,   // S
+    right: 68,  // D
+};
+const pressedKeys = {
+    space: false,
+    up: false,
+    left: false,
+    down: false,
+    right: false,
+};
 
-    const el = document.createElement('li');
-    el.innerHTML = text;
-    document.querySelector('ul').appendChild(el);
+// Handle position updates
+socket.on('update', (pack) => {
+    ctx.clearRect(0, 0, 800, 600);
+    console.log(pack)
+
+    for (const element of pack) {
+        ctx.fillStyle = element.color;
+        ctx.fillRect(element.position.x, element.position.y, element.width, 20);
+    }
 });
 
-document.querySelector('button').onclick = () => {
-    const data = {
-        username: document.getElementsByClassName('username')[0].value,
-        message: document.getElementsByClassName('message')[0].value
+// Handle incoming key presses
+window.addEventListener('keydown', (event) => {
+    if (event.keyCode === availableKeys.space) {
+        pressedKeys.space = true;
     }
-    socket.emit('message', data);
-    document.getElementsByClassName('message')[0].value = '';
-}
+    if (event.keyCode === availableKeys.up) {
+        pressedKeys.up = true;
+    }
+    if (event.keyCode === availableKeys.left) {
+        pressedKeys.left = true;
+    }
+    if (event.keyCode === availableKeys.down) {
+        pressedKeys.down = true;
+    }
+    if (event.keyCode === availableKeys.right) {
+        pressedKeys.right = true;
+    }
+
+    socket.emit('keyPressUpdate', pressedKeys);
+});
+// Handle incoming key releases
+window.addEventListener('keyup', (event) => {
+    if (event.keyCode === availableKeys.space) {
+        pressedKeys.space = false;
+    }
+    if (event.keyCode === availableKeys.up) {
+        pressedKeys.up = false;
+    }
+    if (event.keyCode === availableKeys.left) {
+        pressedKeys.left = false;
+    }
+    if (event.keyCode === availableKeys.down) {
+        pressedKeys.down = false;
+    }
+    if (event.keyCode === availableKeys.right) {
+        pressedKeys.right = false;
+    }
+
+    socket.emit('keyPressUpdate', pressedKeys);
+});
