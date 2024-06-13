1fbdef4fca582a97c09a2980104e853d57f0aeaf
DenizCiger <deniz.ciger@gmx.at>
2024-06-13 17:08:42 +0200

    Update login.php to fix broken stylesheet link

--- php/register.php
+++ php/register.php
@@ -23,7 +23,7 @@ try {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Register</title>
-        <link rel="stylesheet" type="text/css" href="login.css">
+        <link rel="stylesheet" type="text/css" href="../Css/login.css">
     </head> 
 <body>
  
