dae9f676fd26ee97177ae3b50a8053d2db871aa5
DenizCiger <deniz.ciger@gmx.at>
2024-06-17 08:08:52 +0200

    Fix php being outdated

--- client.php
+++ client.php
@@ -7,8 +7,6 @@ if (!isset($_SESSION['loggedin'])) {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,25 +14,46 @@ if (!isset($_SESSION['loggedin'])) {
     <script src="https://cdn.socket.io/4.0.1/socket.io.js"></script> <!-- Socket.io CDN -->
     <script defer src="./js/client.js"></script>
     <style>
-        body {
+        body, html {
+            margin: 0;
+            padding: 0;
             display: flex;
+            flex-direction: column; /* Ensures elements are laid out in a column */
             justify-content: center;
             align-items: center;
             height: 100vh;
-            margin: 0;
-            background-color: rgb(23, 23, 33);
+            background-color: #121212;
+            overflow: hidden;
+            color: white;
         }
         canvas {
             border: 2px solid white;
             background-color: rgb(255, 255, 255);
+            max-height: 90vh;
+            height: auto;
+            width: 100%;
+            max-width: 100vw;
+            display: block;
+            image-rendering: pixelated;
+        }
+        #playerList {
+            display: flex; /* This enables Flexbox for the container */
+            flex-direction: row; /* This makes its children align horizontally */
+            justify-content: center; /* This centers the children horizontally */
+            align-items: center; /* This centers the children vertically */
+            gap: 10px; /* Optional: Adds some space between the children */
         }
         p {
-            color: white;
+            margin: 0px;
+            height: 15px;
+            font-size: 15px;
         }
     </style>
 </head>
 <body>
     <p>Welcome back, <?=htmlspecialchars($_SESSION['username'], ENT_QUOTES)?>!</p>
     <canvas width="480" height="270" id="game"></canvas>
+    <div id="playerList">
+    </div>
 </body>
 </html>
